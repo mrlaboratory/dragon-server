6e4e024c034eddec0916c4ccec8d807655fec5fe
mrlaboratory <mrlaboratory23@gmail.com>
2023-04-29 19:41:10 +0600

    added config for vercel

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 3000
 const cors = require('cors')
 
 const categories = require('./data/categories.json')
+const news = require('./data/news.json')
 
 app.use(cors())
 
@@ -16,6 +17,28 @@ app.get('/categories',(req,res)=> {
 res.send(categories)
 })
 
+app.get('/category/:id',(req,res)=> {
+    const id = req.params.id 
+    if(id == 0){
+        res.send(news)
+    }else{
+        const selectedNews = news.filter(n=> n.category_id === id)
+        res.send(selectedNews)
+    }
+
+
+})
+
+app.get('/news',(req,res)=> {
+res.send(news)
+})
+
+
+app.get('/news/:id',(req,res)=> {
+    const id = req.params.id
+    const selectedNews = news.find(n=> n._id === id)
+    res.send(selectedNews)
+})
 
 app.listen(port,()=> {
     console.log('dragon server is running ')
